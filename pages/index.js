aabeee492f141a104ef596de2d4c2585cbea3121
Abdo Mous <msiAbdrahmn@gmail.com>
2022-10-03 15:38:48 +0100

    remove images

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,6 @@ import {
   AiFillGithub,
 } from "react-icons/ai";
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png";
-import design from "../public/design.png";
-import code from "../public/code.png";
-import consulting from "../public/consulting.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import profilePic from "../public/profile-pic.png";
 import { useState } from "react";
 
